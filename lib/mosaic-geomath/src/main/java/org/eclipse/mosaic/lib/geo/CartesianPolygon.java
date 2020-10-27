2da67bf212569e419bf087d5381ad78bfefa5c40
Pablo Osinaga <pablo.osinaga.arze@fokus.fraunhofer.de>
2020-10-27 10:14:52 +0100

    fix(geomath): CartesianPolygon contains
    Signed-off-by: Pablo Osinaga <pablo.osinaga.arze@fokus.fraunhofer.de>

--- lib/mosaic-geomath/src/main/java/org/eclipse/mosaic/lib/geo/CartesianPolygon.java
+++ lib/mosaic-geomath/src/main/java/org/eclipse/mosaic/lib/geo/CartesianPolygon.java
@@ -15,8 +15,6 @@
 
 package org.eclipse.mosaic.lib.geo;
 
-import org.eclipse.mosaic.lib.math.Vector3d;
-import org.eclipse.mosaic.lib.spatial.Ray;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -79,25 +77,63 @@ public class CartesianPolygon implements Polygon<CartesianPoint>, CartesianArea 
 
     @Override
     public boolean contains(CartesianPoint point) {
-        final Ray rayFromPoint = new Ray();
-        point.toVector3d(rayFromPoint.origin);
-        rayFromPoint.origin.set(point.getX(), 0, -point.getY());
-        rayFromPoint.direction.set(1, 0, 0);
+        float[] verticesXValues = new float[vertices.size()];
+        float[] verticesYValues = new float[vertices.size()];
 
-        int intersections = 0;
-        final Vector3d edgeStart = new Vector3d();
-        final Vector3d edgeEnd = new Vector3d();
-        CartesianPoint prev = null;
-        for (CartesianPoint curr : getVertices()) {
-            if (prev != null) {
-                edgeStart.set(prev.getX(), 0, -prev.getY());
-                edgeEnd.set(curr.getX(), 0, -curr.getY());
-                boolean intersect = rayFromPoint.intersectsLineSegmentXZ(edgeStart, edgeEnd);
-                intersections += intersect ? 1 : 0;
-            }
-            prev = curr;
+        for (int i = 0; i < vertices.size(); i++) {
+            CartesianPoint geoPoint = vertices.get(i);
+            verticesXValues[i] = (float) geoPoint.getX();
+            verticesYValues[i] = (float) geoPoint.getY();
         }
-        return intersections % 2 != 0;
+
+        return pnpoly(vertices.size(), verticesXValues, verticesYValues, (float) point.getX(), (float) point.getY());
+    }
+
+    /** Point Inclusion in Polygon Test.
+     * Defines if a points lies within a polygon
+     * @param nvert Number of vertices in the polygon. Whether to repeat the first vertex at the end is discussed below.
+     * @param vertx Array containing the x-coordinates of the polygon's vertices.
+     * @param verty Array containing the y-coordinates of the polygon's vertices.
+     * @param testx X-coordinate of the test point.
+     * @param testy Y-coordinate of the test point.
+     * @return true if point lies within the polygon, false otherwise
+     */
+    private boolean pnpoly(int nvert, float[] vertx, float[] verty, float testx, float testy) {
+        float minXValue = getMinValue(vertx);
+        float minYValue = getMinValue(verty);
+
+        boolean c = false;
+        for (int i = 0, j = nvert - 1; i < nvert; j = i++) {
+            boolean conditionXAxis;
+            boolean conditionYAxis;
+
+            if (testx <= minXValue) {
+                conditionXAxis = testx < (vertx[j] - vertx[i]) * (testy - verty[i]) / (verty[j] - verty[i]) + vertx[i];
+            } else {
+                conditionXAxis = testx <= (vertx[j] - vertx[i]) * (testy - verty[i]) / (verty[j] - verty[i]) + vertx[i];
+            }
+
+            if (testy <= minYValue) {
+                conditionYAxis = verty[i] > testy != verty[j] > testy;
+            } else {
+                conditionYAxis = verty[i] >= testy != verty[j] >= testy;
+            }
+
+            if (conditionYAxis && conditionXAxis) {
+                c = !c;
+            }
+        }
+        return c;
+    }
+
+    private float getMinValue(float[] numbers) {
+        float minValue = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] < minValue) {
+                minValue = numbers[i];
+            }
+        }
+        return minValue;
     }
 
     public GeoPolygon toGeo() {
