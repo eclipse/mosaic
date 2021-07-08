fbfab6d70a994a2fd052c1380bb8c0bda83ae031
Franziska Vogt <franziska.vogt@fokus.fraunhofer.de>
2021-07-08 12:03:22 +0200

    Renamed package
    Signed-off-by: Franziska Vogt <franziska.vogt@fokus.fraunhofer.de>

--- app/tutorials/example-applications/src/main/java/org/eclipse/mosaic/app/examples/commuter/SimpleCommuterApp.java
+++ app/tutorials/example-applications/src/main/java/org/eclipse/mosaic/app/examples/commuter/SimpleCommuterApp.java
@@ -13,7 +13,7 @@
  * Contact: mosaic@fokus.fraunhofer.de
  */
 
-package org.eclipse.mosaic.app.examples.bikestation;
+package org.eclipse.mosaic.app.examples.commuter;
 
 import org.eclipse.mosaic.fed.application.ambassador.navigation.INavigationModule;
 import org.eclipse.mosaic.fed.application.ambassador.navigation.RoadPositionFactory;
