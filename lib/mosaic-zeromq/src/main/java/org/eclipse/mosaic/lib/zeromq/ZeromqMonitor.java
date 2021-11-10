3581ae2bd3fcd9413026627f0343006b38595bd6
Ongun Türkcüoglu <onguntoglu@gmail.com>
2021-11-10 12:25:59 +0000

    lib: zeromq-monitor started

---
+++ lib/mosaic-zeromq/src/main/java/org/eclipse/mosaic/lib/zeromq/ZeromqMonitor.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2020 Fraunhofer FOKUS and others. All rights reserved.
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contact: mosaic@fokus.fraunhofer.de
+ */
+
+package org.eclipse.mosaic.lib.zeromq;
+
+import org.eclipse.mosaic.rti.api.FederateAmbassador;
+import org.eclipse.mosaic.rti.api.FederationManagement;
+import org.eclipse.mosaic.rti.api.Interaction;
+import org.eclipse.mosaic.rti.api.Monitor;
+import org.eclipse.mosaic.rti.api.TimeManagement;
+import org.eclipse.mosaic.rti.api.time.FederateEvent;
+
+import org.slf4j.Logger;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class ZeromqMonitor implements {
+    
+}
