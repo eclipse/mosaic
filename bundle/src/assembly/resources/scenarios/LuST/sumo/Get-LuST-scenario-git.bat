86fb0899451fb4fcbc661c454266fa5f145ccd16
Ongun Türkcüoglu <onguntoglu@gmail.com>
2021-11-18 22:38:30 +0100

    etc: cleanup

--- bundle/src/assembly/resources/scenarios/LuST/sumo/Get-LuST-scenario-git.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-git clone https://github.com/lcodeca/LuSTScenario.git LuSTScenario
-
-cd LuSTScenario
-git checkout
-cd ..
-
-PAUSE
--- bundle/src/assembly/resources/scenarios/LuST/sumo/Get-LuST-scenario-git.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/lcodeca/LuSTScenario.git ./LuSTScenario
-
-cd ./LuSTScenario
-git checkout
-cd ..
