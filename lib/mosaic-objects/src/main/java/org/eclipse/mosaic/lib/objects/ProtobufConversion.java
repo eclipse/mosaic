f594b88e22e7b7c030d372847aaec9613893b1b2
schwepmo <39306374+schwepmo@users.noreply.github.com>
2021-11-22 10:39:44 +0100

    Add interface for data classes to be serialized with Protobuf (#156)
    Signed-off-by: Moritz Schweppenhäuser <moritz.schweppenhaeuser@fokus.fraunhofer.de>

---
+++ lib/mosaic-objects/src/main/java/org/eclipse/mosaic/lib/objects/ProtobufConversion.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2021 Fraunhofer FOKUS and others. All rights reserved.
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contact: mosaic@fokus.fraunhofer.de
+ */
+
+package org.eclipse.mosaic.lib.objects;
+
+public interface ProtobufConversion<ProtobufT> {
+
+    ProtobufT toProto();
+
+    interface Builder<ProtobufT> {
+        Builder<ProtobufT> fromProto(ProtobufT protobufType);
+    }
+}
