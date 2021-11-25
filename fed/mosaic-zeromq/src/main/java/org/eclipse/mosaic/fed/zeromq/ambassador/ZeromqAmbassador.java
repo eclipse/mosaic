0b9eda8aa302bc18d1356fd8e2b2ff5457eea5a2
Ongun Türkcüoglu <onguntoglu@gmail.com>
2021-11-25 14:50:33 +0000

    feat: server-side publisher and vfd parser added

--- fed/mosaic-zeromq/src/main/java/org/eclipse/mosaic/fed/zeromq/ambassador/ZeromqAmbassador.java
+++ fed/mosaic-zeromq/src/main/java/org/eclipse/mosaic/fed/zeromq/ambassador/ZeromqAmbassador.java
@@ -15,64 +15,37 @@
 
 package org.eclipse.mosaic.fed.zeromq.ambassador;
 
-import org.eclipse.mosaic.interactions.communication.AdHocCommunicationConfiguration;
-import org.eclipse.mosaic.interactions.communication.V2xMessageReception;
-import org.eclipse.mosaic.interactions.communication.V2xMessageTransmission;
-import org.eclipse.mosaic.interactions.mapping.ChargingStationRegistration;
-import org.eclipse.mosaic.interactions.mapping.RsuRegistration;
-import org.eclipse.mosaic.interactions.mapping.TrafficLightRegistration;
 import org.eclipse.mosaic.interactions.traffic.VehicleUpdates;
-import org.eclipse.mosaic.rti.TIME;
 import org.eclipse.mosaic.rti.api.AbstractFederateAmbassador;
-import org.eclipse.mosaic.rti.api.IllegalValueException;
 import org.eclipse.mosaic.rti.api.Interaction;
 import org.eclipse.mosaic.rti.api.InternalFederateException;
 import org.eclipse.mosaic.rti.api.parameters.AmbassadorParameter;
 import org.eclipse.mosaic.lib.objects.vehicle.VehicleData;
-import org.eclipse.mosaic.lib.objects.mapping.ChargingStationMapping;
-import org.eclipse.mosaic.lib.objects.mapping.RsuMapping;
-import org.eclipse.mosaic.lib.objects.mapping.TrafficLightMapping;
 import org.eclipse.mosaic.lib.util.objects.ObjectInstantiation;
+import org.eclipse.mosaic.lib.geo.GeoPoint;
 
 import org.eclipse.mosaic.fed.zeromq.config.CZeromq;
 
-import org.zeromq.ZMsg;
-import org.eclipse.mosaic.lib.zeromq.majordomo.MajordomoBroker;
-import org.eclipse.mosaic.lib.zeromq.majordomo.MajordomoWorker;
-import org.eclipse.mosaic.lib.zeromq.majordomo.MajordomoClient;
-import org.eclipse.mosaic.lib.zeromq.majordomo.MDP;
+import org.zeromq.SocketType;
+import org.zeromq.ZMQ.Socket;
+import org.zeromq.ZContext;
 
 import com.google.gson.Gson;
+
 import java.util.Arrays;
+import java.util.List;
+import java.util.HashMap;
 
 
 public class ZeromqAmbassador extends AbstractFederateAmbassador {
 
     ZContext ctx = new ZContext();
     private final Socket publisher = ctx.createSocket(SocketType.PUB);
-    private final Socket puller = ctx.createSocket(SocketType.PULL);;
     int backendProxyPort;
-    int proxyBackendPort;
-    ZPoller poller = new ZPoller(ctx);
-    Poller items;
 
     public ZeromqAmbassador(AmbassadorParameter ambassadorParameter) {
         super(ambassadorParameter);
     }
-
-    /**
-     * Runs Majordomo Broker in different Thread
-     */
-
-     /**
-      * 
-    @Override
-    public void run() {
-        MajordomoBroker mdpBroker = new MajordomoBroker(true);
-        mdpBroker.bind(this.brokerAddress);
-        mdpBroker.mediate();
-    }
-      */
 
 
     @Override
@@ -87,29 +60,22 @@ public class ZeromqAmbassador extends AbstractFederateAmbassador {
         try {
             CZeromq configuration = new ObjectInstantiation<>(CZeromq.class).readFile(ambassadorParameter.configuration);
             backendProxyPort = configuration.getBackendProxy();
-            proxyBackendPort = configuration.getProxyBackend();
 
         } catch (InstantiationException e) {
             log.error("Could not read configuration. Reason: {}", e.getMessage());
         }
         
-        String backendProxyAddr = "tcp://127.0.0.1:" + String.valueOf(5555);
-        String proxyBackendAddr = "tcp://127.0.0.1:" + String.valueOf(proxyBackendPort);
+        String backendProxyAddr = "tcp://127.0.0.1:" + String.valueOf(backendProxyPort);
         publisher.connect(backendProxyAddr);
-        puller.connect(proxyBackendAddr);
 
         log.info("Initialized Zeromq Sockets!");
     }
 
-        log.info("Initialized MajordomoWorker");
-    }
 
     @Override
     protected void processInteraction(Interaction interaction) throws InternalFederateException {
         try {
-            if (interaction.getTypeId().startsWith(RsuRegistration.TYPE_ID)) {
-                this.process((RsuRegistration) interaction);
-            }  else if (interaction.getTypeId().startsWith(VehicleUpdates.TYPE_ID)) {
+            if (interaction.getTypeId().startsWith(VehicleUpdates.TYPE_ID)) {
                 this.process((VehicleUpdates) interaction);
             } else {
             }
@@ -118,23 +84,31 @@ public class ZeromqAmbassador extends AbstractFederateAmbassador {
         }
     }
 
-    private void process(RsuRegistration interaction) {
-        final RsuMapping applicationRsu = interaction.getMapping();
-        if (applicationRsu.hasApplication()) {
-            log.info("Added RSU id={} position={} @time={}", applicationRsu.getName(), applicationRsu.getPosition(), TIME.format(interaction.getTime()));
-        }
-    }
-
     private void process(VehicleUpdates interaction) {
             String json = this.createFVDGson(interaction);
             publisher.send(json);
     }
 
-    }
+    private List<Double> geoPointConvert(GeoPoint pos){
+        List<Double> list = Arrays.asList(pos.getLatitude(), pos.getLongitude());
+        return list;
+    } 
 
-    private String createInteractionGsonStr(VehicleUpdates interaction){
+    private String createFVDGson(VehicleUpdates interaction){
         Gson gson = new Gson();
-        String json = gson.toJson(interaction);
+        HashMap<String, Object> updatedSingle = new HashMap<String, Object>();
+        HashMap<String, Object> updatedHash = new HashMap<String, Object>();
+        List<VehicleData> updated = interaction.getUpdated();
+
+        for (VehicleData vehicle : updated){
+            updatedSingle.put("time", vehicle.getTime());
+            updatedSingle.put("position",geoPointConvert(vehicle.getPosition()));
+            updatedSingle.put("speed", vehicle.getSpeed());
+            updatedSingle.put("road_id", vehicle.getRoadPosition().getConnectionId());
+
+            updatedHash.put(vehicle.getName(), updatedSingle);
+        }
+        String json = gson.toJson(updatedHash);
         return json;
     }
 
