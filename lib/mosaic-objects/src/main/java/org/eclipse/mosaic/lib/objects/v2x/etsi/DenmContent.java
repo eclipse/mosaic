5e00a27ca78288f64ca73d9d5187b2f4675f97a9
nin62743 <nina.boehm@fokus.fraunhofer.de>
2022-02-21 13:53:41 +0100

    remove blank line

--- lib/mosaic-objects/src/main/java/org/eclipse/mosaic/lib/objects/v2x/etsi/DenmContent.java
+++ lib/mosaic-objects/src/main/java/org/eclipse/mosaic/lib/objects/v2x/etsi/DenmContent.java
@@ -189,7 +189,6 @@ public class DenmContent implements ToDataOutput, Serializable {
     @Override
     public void toDataOutput(DataOutput dataOutput) throws IOException {
         dataOutput.writeLong(time);
-
         dataOutput.writeBoolean(senderPosition != null);
         if (senderPosition != null) {
             SerializationUtils.encodeGeoPoint(dataOutput, senderPosition);
