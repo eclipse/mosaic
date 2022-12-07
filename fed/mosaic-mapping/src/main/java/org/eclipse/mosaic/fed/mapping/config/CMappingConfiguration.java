4a903ccefd0341090aa7c78dceb2092d00dad738
schwepmo <39306374+schwepmo@users.noreply.github.com>
2022-12-07 09:51:30 +0100

    feat(mapping): Define starting times using string definitions (#277)
    It is now possible to use string definitions for double-typed time values using the DoubleTimeFieldAdapters
    * all bundled scenarios will now use starting time definitions in the form of "x.x s"
    * additionally some occurrences in test mapping-configs now use a string definition
    
    Signed-off-by: Moritz Schweppenhaeuser <moritz.schweppenhaeuser@fokus.fraunhofer.de>

--- fed/mosaic-mapping/src/main/java/org/eclipse/mosaic/fed/mapping/config/CMappingConfiguration.java
+++ fed/mosaic-mapping/src/main/java/org/eclipse/mosaic/fed/mapping/config/CMappingConfiguration.java
@@ -16,6 +16,10 @@
 package org.eclipse.mosaic.fed.mapping.config;
 
 
+import org.eclipse.mosaic.lib.util.gson.TimeFieldAdapter;
+
+import com.google.gson.annotations.JsonAdapter;
+
 /**
  * Class that contains options for the parametrization of the Mapping.
  */
@@ -28,27 +32,29 @@ public class CMappingConfiguration {
     public double scaleTraffic = 1.0;
 
     /**
-     * Defines the point in time (in seconds) to start spawning vehicles. If not set (default),
-     * all vehicles will be spawned according to the vehicles configuration.
+     * Defines the point in time to start spawning vehicles. If not set (default),
+     * all vehicles will be spawned according to the vehicles configuration. [s]
      */
+    @JsonAdapter(TimeFieldAdapter.DoubleSecondsNullable.class)
     public Double start;
 
     /**
-     * Defines the point in time (in seconds) to end spawning vehicles. If not set (default),
-     * all vehicles will be spawned according to the vehicles configuration or until the simulation ends.
+     * Defines the point in time to end spawning vehicles. If not set (default),
+     * all vehicles will be spawned according to the vehicles configuration or until the simulation ends. [s]
      */
+    @JsonAdapter(TimeFieldAdapter.DoubleSecondsNullable.class)
     public Double end;
 
     /**
-     * If set to <code>true</code> and if the parameter <code>start</code> is set, the starting
+     * If set to {@code true} and if the parameter {@code start} is set, the starting
      * times of each spawner is adjusted accordingly, so that we shouldn't wait
      * in case that simulation starting time and spawner starting time are widely spread out.
-     * All spawners before <code>start</code> will be completely ignored then.
+     * All spawners before {@code start} will be completely ignored then.
      */
     public boolean adjustStartingTimes = false;
 
     /**
-     * If set to <code>true</code>, all flow definitions defined by vehicle spawners with more than 1 vehicle
+     * If set to {@code true}, all flow definitions defined by vehicle spawners with more than 1 vehicle
      * result in slightly randomized departure times. The specified `targetFlow` of the vehicle spawner is kept.
      */
     public boolean randomizeFlows = false;
