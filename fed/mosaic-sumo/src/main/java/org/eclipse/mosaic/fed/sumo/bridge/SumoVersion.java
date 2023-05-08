e3506c5c5f049157be513601d6e5c39f0570c369
Karl Schrab <karl.schrab@fokus.fraunhofer.de>
2023-05-08 09:44:48 +0200

    feat(sumo): upgrade to SUMO 1.17.0 (#318)
    * feat: upgrade to SUMO 1.17.0
    * fix(sumo): allow lower step size than 100ms

--- fed/mosaic-sumo/src/main/java/org/eclipse/mosaic/fed/sumo/bridge/SumoVersion.java
+++ fed/mosaic-sumo/src/main/java/org/eclipse/mosaic/fed/sumo/bridge/SumoVersion.java
@@ -42,6 +42,8 @@ public enum SumoVersion {
     SUMO_1_15_x("1.15.*", TraciVersion.API_20),
     SUMO_1_16_x("1.16.*", TraciVersion.API_20),
 
+    SUMO_1_17_x("1.16.*", TraciVersion.API_20),
+
     /**
      * the lowest version supported by this client.
      */
@@ -50,7 +52,7 @@ public enum SumoVersion {
     /**
      * the highest version supported by this client.
      */
-    HIGHEST(SUMO_1_16_x.sumoVersion, SUMO_1_16_x.traciVersion);
+    HIGHEST(SUMO_1_17_x.sumoVersion, SUMO_1_17_x.traciVersion);
 
     private final String sumoVersion;
     private final TraciVersion traciVersion;
