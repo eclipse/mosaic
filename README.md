b91c9353e04414a74830c8ad6b18eaa24fb65d39
Moritz Schweppenhäuser <moritz.schweppenhaeuser@fokus.fraunhofer.de>
2023-07-05 16:28:35 +0200

    chore(sumo): update to sumo 1.18.0
    Signed-off-by: Moritz Schweppenhäuser <moritz.schweppenhaeuser@fokus.fraunhofer.de>

--- README.md
+++ README.md
@@ -61,7 +61,7 @@ For a successful build you need the following software to be installed:
 
 * **Maven 3.1.x** or higher.
 * **Java 8, 11, or 17** - We recommend using the [Adoptium OpenJDK (aka Eclipse Temurin)](https://adoptium.net/?variant=openjdk8).
-* **SUMO 1.17.0** - Additionally, the environment variable `SUMO_HOME` should be configured properly.
+* **SUMO 1.18.0** - Additionally, the environment variable `SUMO_HOME` should be configured properly.
 
 ## Build
 
--- fed/mosaic-sumo/src/main/java/org/eclipse/mosaic/fed/sumo/bridge/SumoVersion.java
+++ fed/mosaic-sumo/src/main/java/org/eclipse/mosaic/fed/sumo/bridge/SumoVersion.java
@@ -41,8 +41,8 @@ public enum SumoVersion {
     SUMO_1_14_x("1.14.*", TraciVersion.API_20),
     SUMO_1_15_x("1.15.*", TraciVersion.API_20),
     SUMO_1_16_x("1.16.*", TraciVersion.API_20),
-
     SUMO_1_17_x("1.17.*", TraciVersion.API_20),
+    SUMO_1_18_x("1.18.*", TraciVersion.API_20),
 
     /**
      * the lowest version supported by this client.
@@ -52,7 +52,7 @@ public enum SumoVersion {
     /**
      * the highest version supported by this client.
      */
-    HIGHEST(SUMO_1_17_x.sumoVersion, SUMO_1_17_x.traciVersion);
+    HIGHEST(SUMO_1_18_x.sumoVersion, SUMO_1_18_x.traciVersion);
 
     private final String sumoVersion;
     private final TraciVersion traciVersion;
