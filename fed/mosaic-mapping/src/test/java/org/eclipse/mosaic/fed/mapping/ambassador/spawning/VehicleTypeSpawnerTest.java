b184d3fd211b48f1cceab1936dbf4dce4c8c1a5e
Karl Schrab <karl.schrab@fokus.fraunhofer.de>
2023-08-01 16:42:31 +0200

    fix: decel was missed when creating a vehicle type (#337)

--- fed/mosaic-mapping/src/test/java/org/eclipse/mosaic/fed/mapping/ambassador/spawning/VehicleTypeSpawnerTest.java
+++ fed/mosaic-mapping/src/test/java/org/eclipse/mosaic/fed/mapping/ambassador/spawning/VehicleTypeSpawnerTest.java
@@ -19,13 +19,61 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 import org.eclipse.mosaic.fed.mapping.config.CPrototype;
+import org.eclipse.mosaic.lib.enums.LaneChangeMode;
+import org.eclipse.mosaic.lib.enums.SpeedMode;
 import org.eclipse.mosaic.lib.enums.VehicleClass;
 import org.eclipse.mosaic.lib.math.DefaultRandomNumberGenerator;
+import org.eclipse.mosaic.lib.math.MathUtils;
 import org.eclipse.mosaic.lib.math.RandomNumberGenerator;
+import org.eclipse.mosaic.lib.objects.vehicle.VehicleType;
 
 import org.junit.Test;
 
 public class VehicleTypeSpawnerTest {
+
+    @Test
+    public void creationFromCPrototype() {
+        // SETUP
+        CPrototype prototype = new CPrototype();
+        prototype.weight = 0.1;
+
+        prototype.length = 5.0;
+        prototype.width = 2.0;
+        prototype.height = 1.5;
+
+        prototype.minGap = 2.3;
+        prototype.tau = 1.0;
+        prototype.sigma = 0.2;
+
+        prototype.accel = 2.3;
+        prototype.decel = 2.5;
+        prototype.maxSpeed = 30.0;
+        prototype.emergencyDecel = 7.4;
+        prototype.speedFactor = 1.2;
+
+        prototype.vehicleClass = VehicleClass.AutomatedVehicle;
+        prototype.speedMode = SpeedMode.SPEEDER;
+        prototype.laneChangeMode = LaneChangeMode.COOPERATIVE;
+
+        // RUN
+        VehicleType type = new VehicleTypeSpawner(prototype).convertType();
+
+        // ASSERT
+        assertEquals(prototype.length, type.getLength(), MathUtils.EPSILON_D);
+        assertEquals(prototype.width, type.getWidth(), MathUtils.EPSILON_D);
+        assertEquals(prototype.height, type.getHeight(), MathUtils.EPSILON_D);
+        assertEquals(prototype.minGap, type.getMinGap(), MathUtils.EPSILON_D);
+        assertEquals(prototype.tau, type.getTau(), MathUtils.EPSILON_D);
+        assertEquals(prototype.sigma, type.getSigma(), MathUtils.EPSILON_D);
+        assertEquals(prototype.accel, type.getAccel(), MathUtils.EPSILON_D);
+        assertEquals(prototype.decel, type.getDecel(), MathUtils.EPSILON_D);
+        assertEquals(prototype.maxSpeed, type.getMaxSpeed(), MathUtils.EPSILON_D);
+        assertEquals(prototype.emergencyDecel, type.getEmergencyDecel(), MathUtils.EPSILON_D);
+        assertEquals(prototype.speedFactor, type.getSpeedFactor(), MathUtils.EPSILON_D);
+        assertEquals(prototype.vehicleClass, type.getVehicleClass());
+        assertEquals(prototype.speedMode, type.getSpeedMode());
+        assertEquals(prototype.laneChangeMode, type.getLaneChangeMode());
+    }
 
     @Test
     public void fillInPrototype() {
