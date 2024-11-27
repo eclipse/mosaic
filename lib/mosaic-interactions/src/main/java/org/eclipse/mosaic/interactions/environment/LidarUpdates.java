5a571ed85cc8202712a0e325fce830225792dee4
Finn Schneider <finn.schneider@fokus.fraunhofer.de>
2024-11-27 15:38:13 +0100

    fix(interaction): removed unused imports, fixed calls

--- lib/mosaic-interactions/src/main/java/org/eclipse/mosaic/interactions/environment/LidarUpdates.java
+++ lib/mosaic-interactions/src/main/java/org/eclipse/mosaic/interactions/environment/LidarUpdates.java
@@ -24,9 +24,7 @@ import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class LidarUpdates extends Interaction {
